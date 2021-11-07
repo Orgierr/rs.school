777c742eedaec95978733e970d859d013a6765e9
Igor.K <infinitemind625@gmail.com>
2021-11-07 22:38:08 +0200

    feat:add custom transform stream

---
+++ app/custom_transform_stream.js
@@ -0,0 +1,46 @@
+const { Transform } = require('stream');
+const atbash = require('./atbash');
+const encode = require('./encode');
+const decode = require('./decode');
+
+class CustomTransform extends Transform {
+  constructor(cipher) {
+    super();
+    this.cipher = cipher;
+  }
+  _transform(chunk, encoding, callback) {
+    const data = chunk
+      .toString()
+      .split('')
+      .map((letter) => transformLetter(letter, this.cipher))
+      .join('');
+    callback(null, data);
+  }
+}
+
+function alphCase(letter, alphabet) {
+  if (alphabet.toLowerCase().includes(letter)) return alphabet;
+  return alphabet.toUpperCase();
+}
+
+function transformLetter(letter, cipher) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const alphLen = 26;
+  if (!alphabet.toLowerCase().includes(letter.toLowerCase())) return letter;
+  const letterIndex = alphabet.indexOf(letter.toLowerCase());
+  const newAlphabet = alphCase(letter, alphabet);
+  switch (cipher) {
+    case 'A':
+      return atbash(letterIndex, newAlphabet);
+    case 'C1':
+      return encode(1, newAlphabet, alphLen, letterIndex);
+    case 'R1':
+      return encode(8, newAlphabet, alphLen, letterIndex);
+    case 'C0':
+      return decode(1, newAlphabet, letterIndex);
+    case 'R0':
+      return decode(8, newAlphabet, letterIndex);
+  }
+}
+
+module.exports = CustomTransform;
