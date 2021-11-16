1b392665b28bbae34301f2711aa176e400a1be06
Igor.K <infinitemind625@gmail.com>
2021-11-16 19:47:24 +0200

    feat:add transforme letter test

--- __test__/transform_letter.test.js
+++ __test__/transform_letter.test.js
@@ -1,5 +1,4 @@
 const transformLetter = require('../app/transform_letter');
-const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 test('transform letter', () => {
   expect(transformLetter('a', 'C1')).toBe('b');
