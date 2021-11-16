c2890aadf39ee31b2957a6a81136805f23fa51df
Igor.K <infinitemind625@gmail.com>
2021-11-16 20:00:52 +0200

    feat:add custom_transform_stream.test

--- app/custom_transform_stream.js
+++ app/custom_transform_stream.js
@@ -1,12 +1,13 @@
 const { Transform } = require('stream');
 
 const transformLetter = require('./transform_letter');
-
+/* istanbul ignore next */
 class CustomTransform extends Transform {
   constructor(cipher) {
     super();
     this.cipher = cipher;
   }
+  /* istanbul ignore next */
   _transform(chunk, encoding, callback) {
     const data = chunk
       .toString()
--- jest.config.js
+++ jest.config.js
@@ -23,15 +23,16 @@ module.exports = {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
+  //   '\\\\node_modules\\\\',
+  //   'custom_transform_stream',
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
